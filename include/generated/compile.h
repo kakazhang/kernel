8a81d0835c63d259f7087b2c3aa58fe3448e1e16
zhanglin <zhanglin3406@gmail.com>
2016-08-21 20:53:45 +0800

    kernel code of tegra from nvdia

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 17 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#17 SMP PREEMPT Sun Aug 21 20:28:23 CST 2016"
+#define LINUX_COMPILE_BY "linzi"
+#define LINUX_COMPILE_HOST "linzi"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.1.10-gc7133fe-dirty"
